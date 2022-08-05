6b8873eb00b14550524f5a11ab95b8470177d150
Max <79068976350@yandex.ru>
2022-08-05 20:15:10 +0400

    add history checks

--- tables/history.sql
+++ tables/history.sql
@@ -1,11 +1,11 @@
 CREATE TABLE "history"
 (
-    "primary_key" JSONB,
+    "primary_key" JSONB
+        CONSTRAINT "check_primary_key" CHECK ( ("dml" = 'INSERT' AND "primary_key" IS NULL) OR ("primary_key" IS NOT NULL AND "primary_key" != '{}' AND jsonb_typeof("primary_key") = 'object') ),
     "dml"         DML       NOT NULL,
-    "data"        JSONB     NOT NULL, -- TODO ADD CHECKS
+    "data"        JSONB
+        CONSTRAINT "check_data" CHECK ( ("dml" = 'DELETE' AND "data" IS NULL) OR ("data" IS NOT NULL AND "data" != '{}' AND jsonb_typeof("data") = 'object') ),
     "timestamp"   TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 COMMENT ON TABLE "history" IS '';
-
--- TODO REFERENCE ON UPDATE CASCADE ???
