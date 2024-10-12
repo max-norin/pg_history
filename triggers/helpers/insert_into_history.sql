a53f675af3ffbdec4f1cbd5927f9d52d00f6cb07
max_norin <79068976350@yandex.ru>
2024-10-12 23:43:44 +0300

    fix insert_into_history

--- triggers/helpers/insert_into_history.sql
+++ triggers/helpers/insert_into_history.sql
@@ -5,7 +5,7 @@ $$
 DECLARE
     "result" JSONB NOT NULL = '{}';
 BEGIN
-    EXECUTE format('INSERT INTO %1s ("primary_key", "dml", "data") VALUES (%2L,%3L,%4L,%5L) RETURNING to_json(%1s.*);',
+    EXECUTE format('INSERT INTO %1s ("primary_key", "dml", "data") VALUES (%2L,%3L,%4L) RETURNING to_json(%1s.*);',
                    "target_table",
                    "primary_key", "dml", "data",
                    "target_table"
